14ff6e00bbfd1bee7caffd458f75b2a5bd42bdd9
Detergent13 <50428606+Detergent13@users.noreply.github.com>
2020-11-23 19:02:32 -0600

    Update basic_bot.py

--- basic_bot.py
+++ basic_bot.py
@@ -7,7 +7,7 @@ import os
 
 client= discord.Client()
 lolList= ["Time to grind League of Legends","Do not play League of Legends"]
-takashi_list = ["😑", ":expressionless:", ":bread:", "-_-", ":unamused:", ":neutral_face:", ":"]
+takashi_list = ["-_-",":"]
 
 @client.event
 async def on_ready():
